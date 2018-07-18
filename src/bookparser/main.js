0229a587a332af2be4e3617c82e7bc5833e2c3a0
Cristian Gomez <cristiangomezmendez@gmail.com>
2018-07-18 19:59:04 -0400

    feature: book parser tool

--- src/bookparser/main.js
+++ src/bookparser/main.js
@@ -4,47 +4,71 @@ const parser = require('./parser');
 
 const basePath = __dirname + '/../../sampleData/';
 const authorsFile = basePath + '/../sampleDataJson/authors.json';
+const booksFile = basePath + '/../sampleDataJson/books.json';
 
-fs.readdir(basePath, (err, authors) => {
+function main() {
+  const authorsNames = fs.readdirSync(basePath);
   let currentAuthors = JSON.parse(fs.readFileSync(authorsFile).toString());
-  console.log(currentAuthors);
-  const maxId = Math.max.apply(Math, currentAuthors.map((author) => author.id));
-  let nextId = maxId + 1;
-  for (const author of authors) {
-    let authorModel = currentAuthors.find((existentAuthor) => {
-      return author == existentAuthor.name;
-    });
-    if (!authorModel) {
-      authorModel = {
-        id: nextId,
-        name: author
-      };
-      currentAuthors.push(authorModel);
-    }
-    fs.writeFileSync(authorsFile, JSON.stringify(currentAuthors));
-    
-    fs.readdir(basePath + author, (err, books) => {
-      for (const book of books) {
-        const bookPath = basePath + author + '/' + book + '/';
-        text = stripHeaders.stripHeaders(fs.readFileSync(bookPath + 'book.txt').toString());
-        html = stripHeaders.stripHeaders(fs.readFileSync(bookPath + 'book.html').toString());
-        htmlPages = parser.getHtmlPages(html);
-        textPages = parser.getTextPages(text);
-        let pagesModels = [];
-        for (let i = 0; i < htmlPages.length; i++) {
-          pagesModels.push({
-            bookId: 1,
-            html: htmlPages[i],
-            text: textPages[i],
-            updatedAt: new Date(),
-            createdAt: new Date()
-          });
-        }
-        const bookModel = {
-          id: 1,
-          authorId: 1
+  let nextAuthorId = getNextId(currentAuthors);
+  for (const authorName of authorsNames) {
+    const author = getAuthorModel(nextAuthorId, authorName, currentAuthors);
+
+    const booksNames = fs.readdirSync(basePath + authorName);
+
+    for (const bookName of booksNames) {
+      let currentBooks = JSON.parse(fs.readFileSync(booksFile).toString());
+      let nextBookId = getNextId(currentBooks);
+
+      let bookModel = currentBooks.find((existentBook) => {
+        return bookName == existentBook.title && author.id == existentBook.id;
+      });
+
+      if (!bookModel) {
+        bookModel = {
+          id: nextBookId,
+          title: bookName,
+          authorId: author.id
         };
+        currentBooks.push(bookModel);
+        nextAuthorId += 1;
+        fs.writeFileSync(booksFile, JSON.stringify(currentBooks));
       }
-    });
+
+      const bookPath = basePath + authorName + '/' + bookName + '/';
+      text = stripHeaders.stripHeaders(fs.readFileSync(bookPath + 'book.txt').toString());
+      html = stripHeaders.stripHeaders(fs.readFileSync(bookPath + 'book.html').toString());
+      htmlPages = parser.getHtmlPages(html);
+      textPages = parser.getTextPages(text);
+      let pagesModels = [];
+      for (let i = 0; i < htmlPages.length; i++) {
+        pagesModels.push({
+          bookId: 1,
+          html: htmlPages[i],
+          text: textPages[i],
+          updatedAt: new Date(),
+          createdAt: new Date()
+        });
+      }
+    }
   }
-});
+}
+
+function getAuthorModel(nextAuthorId, authorName, currentAuthors) {
+  let authorModel = currentAuthors.find((existentAuthor) => {
+    return authorName == existentAuthor.name;
+  });
+  if (!authorModel) {
+    authorModel = {
+      id: nextAuthorId,
+      name: authorName
+    };
+    currentAuthors.push(authorModel);
+    nextAuthorId += 1;
+    fs.writeFileSync(authorsFile, JSON.stringify(currentAuthors));
+  }
+  return authorModel;
+}
+
+function getNextId(models) {
+  return models.length > 0 ? Math.max.apply(Math, models.map((model) => model.id)) + 1 : 1;
+}
